5ce154c07e5c103c6cabd639b7ee3ec73d06527b
wcht <wcht@WCHT-PC.ad.ds>
2019-09-24 21:30:22 +0200

    My Second commit

---
+++ test/src/classes/MaClasse.java
@@ -0,0 +1,13 @@
+package classes;
+
+
+public class MaClasse {
+	
+	int numb=10;
+	public void incrementer() {
+		System.out.println("Le nombre actuel est : ");
+		numb++;
+		System.out.println("Le nombre incrémenté est : " + numb);
+	}
+
+}
